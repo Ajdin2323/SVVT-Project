be3349654754f78ed94739c546e9438597a1d02c
Ajdin <ajdin.karavdic@stu.ibu.ba>
2024-01-07 23:37:26 +0100

    project

--- core/page-objects/register-page.ts
+++ core/page-objects/register-page.ts
@@ -14,7 +14,7 @@ export class Register extends BasePage {
     private xButton = By.xpath("//button[@class='a83ed08757 c21c56c305 f38b6daa18 d691166b09 ab98298258 deab83296e f4552b6561']");
     private registerButton = By.xpath("//a[@data-testid='header-sign-up-button']");
     private mailInputField = By.xpath("//button[@class='Iiab0gVMeWOd4XcyJGA3 wPxWIS_rJCpwAWksE0s3 Ut3prtt_wDsi7NM_83Jc TuDOVH9WFSdot9jLyXlw EJWUAldA4O1mP0SSFXPm whxYYRnvyHGyGqxO4ici']");
-    private ContinueButton = By.xpath("//button[@class='Iiab0gVMeWOd4XcyJGA3 wPxWIS_rJCpwAWksE0s3 Ut3prtt_wDsi7NM_83Jc TuDOVH9WFSdot9jLyXlw EJWUAldA4O1mP0SSFXPm whxYYRnvyHGyGqxO4ici']");
+    private ContinueButton = By.xpath("//button[@class='Iiab0gVMeWOd4XcyJGA3 wPxWIS_rJCpwAWksE0s3 Ut3prtt_wDsi7NM_83Jc TuDOVH9WFSdot9jLyXlw EJWUAldA4O1mP0SSFXPm whxYYRnvyHGyGqxO4ici']/span[@class='yfCvx60qsR50VNBG15jF']");
     private passInputField = By.xpath("//input[@class='ZMH9h0HCYH9GGNxHnXGJ']");
     private SignInButton = By.xpath("class='Iiab0gVMeWOd4XcyJGA3 wPxWIS_rJCpwAWksE0s3 Ut3prtt_wDsi7NM_83Jc TuDOVH9WFSdot9jLyXlw EJWUAldA4O1mP0SSFXPm whxYYRnvyHGyGqxO4ici'");
     
